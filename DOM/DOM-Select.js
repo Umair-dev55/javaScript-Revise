ed3ca9b4a0e3b6f5dd0d65965262d8a7b7a4587b
Umair Ali <umair.personal55@gmail.com>
2025-10-20 20:52:34 +0500

    Basics Practise

---
+++ DOM/DOM-Select.js
@@ -0,0 +1,8 @@
+// Agr ap Seperately ik ik ko Select kro 
+// document.getElementById("heading-first").innerText = "DOM Selector";
+// document.getElementById("heading-second").innerText = "in javaScript";
+// document.getElementsByClassName("btn")[0].innerText = "Contact";
+
+// Mow Select kro
+// document.querySelector(".heading").innerText = "Contact Us";
+// document.querySelectorAll(".heading").innerText = "Contact Us";
---
+++ DOM/DOM.js
@@ -0,0 +1,31 @@
+// console.dir(Window);
+// console.dir(Document);
+// console.dir(Document);
+// console.dir(DocumentType);
+// console.dir(Node);
+// console.dir(Element);
+// console.dir(Attr);
+// console.dir(Text);
+// console.dir(Comment);
+// console.dir(DocumentFragment)
+
+// document.body.style.backgroundColor = "red";
+document.body.childNodes[3].innerText = "Hello"; //change text in HTML
+document.body.style.background = "black"; //change background color
+console.log(document.body.childNodes[1]); //1st Child node in body
+// document.getElementsByClassName("#heading")
+// document.getElementsByClassName(".heading")
+// console.log(Window);
+// console.log(Document);
+// console.log(DocumentType);
+// console.log(Node);
+// console.log(Element);
+// console.log(Attr);
+// console.log(Text);
+// console.log(Comment);
+// console.log(DocumentFragment)
+
+//---------DOM MANIPULATION--------//
+let heading=document.getElementById("heading");
+console.dir(heading);
+// document.getElementById("heading").style.color = "red";//Direct Changes in HTML file
