103213ff42562702187ed986f0757eb6c3912e97
Umair Ali <umair.personal55@gmail.com>
2025-11-01 10:12:14 +0500

    Using ES6 module Import/export

--- ES6 Modules – import & export/script.js
+++ ES6 Modules – import & export/script.js
@@ -1,11 +1,19 @@
+//----Named Import-----
 import { b } from "./app.js";
 console.log(b);
 import { arr } from "./app.js";
 console.log(arr);
-
+//Default Import-------
 import user, { user2, user3 } from "./app.js";
 
-// Print karne ke liye
+// Print 
 console.log(user);   // { name: "John", age: 20, city: "Lahore" }
 console.log(user2);  // { name: "Ali", age: 20, city: "Lahore" }
 console.log(user3);  // { name: "Ahmed", age: 20, city: "Lahore" }
+
+//Output
+// 20
+// (10) [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// {name: 'John', age: 20, city: 'lahore'}
+// {name: 'Ali', age: 20, city: 'lahore'}
+// {name: 'Ahmed', age: 20, city: 'lahore'}•
