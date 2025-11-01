c4685a6aa2fcb1ab850bbc6d635f73fed211a9dd
Umair Ali <umair.personal55@gmail.com>
2025-11-01 08:58:53 +0500

    ES6 Modules – import & export

---
+++ ES6 Modules – import & export/script.js
@@ -0,0 +1,11 @@
+import { b } from "./app.js";
+console.log(b);
+import { arr } from "./app.js";
+console.log(arr);
+
+import user, { user2, user3 } from "./app.js";
+
+// Print karne ke liye
+console.log(user);   // { name: "John", age: 20, city: "Lahore" }
+console.log(user2);  // { name: "Ali", age: 20, city: "Lahore" }
+console.log(user3);  // { name: "Ahmed", age: 20, city: "Lahore" }
