2c461220b00d77f883a843b8dfdbe3bd0de9b10f
Umair Ali <umair.personal55@gmail.com>
2025-11-01 11:24:48 +0500

    name change

---
+++ ES6 Modules/app.js
@@ -0,0 +1,23 @@
+export const b = 20;
+export const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const user = {
+  name: "John",
+  age: 20,
+  city: "lahore",
+};
+export default user;
+export const user2 = {
+  name: "Ali",
+  age: 20,
+  city: "lahore",
+};
+export const user3 = {
+  name: "Ahmed",
+  age: 20,
+  city: "lahore",
+};
+
+//Prcatise
+// export const arr=[10,20,30,40];
+// const user=
--- ES6 Modules/script.js
+++ ES6 Modules/script.js
@@ -6,10 +6,10 @@ console.log(arr);
 //Default Import-------
 import user, { user2, user3 } from "./app.js";
 
-// Print 
-console.log(user);   // { name: "John", age: 20, city: "Lahore" }
-console.log(user2);  // { name: "Ali", age: 20, city: "Lahore" }
-console.log(user3);  // { name: "Ahmed", age: 20, city: "Lahore" }
+// Print
+console.log(user); // { name: "John", age: 20, city: "Lahore" }
+console.log(user2); // { name: "Ali", age: 20, city: "Lahore" }
+console.log(user3); // { name: "Ahmed", age: 20, city: "Lahore" }
 
 //Output
 // 20
